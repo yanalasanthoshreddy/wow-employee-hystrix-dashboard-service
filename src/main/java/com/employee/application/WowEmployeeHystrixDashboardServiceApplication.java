4bfc4a512a368784af40ca4fd1cc805faa2661ae
yanalasanthoshreddy <santhosh.201316@gmail.com>
2021-03-02 14:04:02 +0530

    initial commit

---
+++ src/main/java/com/employee/application/WowEmployeeHystrixDashboardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.employee.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowEmployeeHystrixDashboardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowEmployeeHystrixDashboardServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employee/application/WowEmployeeHystrixDashboardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowEmployeeHystrixDashboardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
